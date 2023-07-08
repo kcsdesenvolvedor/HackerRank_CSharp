52d04d64e9b04ceeeafe96f74b13f4b8353ad116
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2023-07-08 11:10:29 -0300

    criando novo modelo de estrutura para os desafios

---
+++ C#/Algorithms/BetweenTwoSets/BetweenTwoSetsStart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.BetweenTwoSets
+{
+    public static class BetweenTwoSetsStart
+    {
+        public static void Start()
+        {
+            var firstArray = new List<int>() { 2, 4 };
+            var secundArray = new List<int>() { 16, 32, 96 };
+            if (BetweenTwoSetsService.Validate(firstArray) && BetweenTwoSetsService.Validate(secundArray))
+                Console.WriteLine(BetweenTwoSetsService.Start(firstArray, secundArray));
+        }
+    }
+}
--- C#/Algorithms/Program.cs
+++ C#/Algorithms/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Algorithms.BetweenTwoSets;
 
-var firstArray = new List<int>() { 2, 4 };
-var secundArray = new List<int>() { 16, 32, 96 };
-if (BetweenTwoSetsService.Validate(firstArray) && BetweenTwoSetsService.Validate(secundArray))
-    Console.WriteLine(BetweenTwoSetsService.Start(firstArray, secundArray));
+// Em cada pasta de desafio temos o arquivo Service e o Start, no Service está a logica do desafio, e no Start
+// tem o metodo Start() que testa o desafio.
+// O arquivo Start é uma classe static que permite chamar diretamente o metodo Start. ex: BetweenTwoSetsStart.Start();
+
+
+BetweenTwoSetsStart.Start();
 
