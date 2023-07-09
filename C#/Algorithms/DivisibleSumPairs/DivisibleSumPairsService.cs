ae6fa500de40f12afa16dd1facc8b7bd60723b0e
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2023-07-09 02:12:43 -0300

    Conclusão do desafio Divisible Sum Pairs

---
+++ C#/Algorithms/DivisibleSumPairs/DivisibleSumPairsService.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.DivisibleSumPairs
+{
+    public static class DivisibleSumPairsService
+    {
+        public static int Start(int arrayCount, int divisor, List<int> array)
+        {
+            var divisonResult = 0;
+            for(var i = 0; i < array.Count -1; i++)
+            {
+                for(var j = i+1; j < array.Count; j++)
+                {
+                    var sumResult = array[i] + array[j];
+                    if (sumResult % divisor == 0)
+                        divisonResult++;
+                }
+            }
+            return divisonResult;
+        }
+
+        public static bool Validate(int arrayCount, int divisor, List<int> array)
+        {
+            if ((arrayCount < 2 || arrayCount > 100) ||
+                (divisor < 1 || divisor > 100)) 
+                    return false;
+
+            foreach (var number in array)
+            {
+                if (number < 1 || number > 100) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ C#/Algorithms/DivisibleSumPairs/DivisibleSumPairsStart.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithms.DivisibleSumPairs
+{
+    public static class DivisibleSumPairsStart
+    {
+        public static void Start()
+        {
+            var array = new List<int>() { 1, 3, 2, 6, 1, 2 };
+            Console.WriteLine(DivisibleSumPairsService.Start(6, 3, array));
+        }
+    }
+}
