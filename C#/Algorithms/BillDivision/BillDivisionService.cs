34d2801ae7bc58740806a05cacbe1c13dfd87c93
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2023-07-14 23:01:38 -0300

    Concluindo desafio Bill Division

---
+++ C#/Algorithms/BillDivision/BillDivisionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.BillDivision
+{
+    public static class BillDivisionService
+    {
+        public static string Start(List<int> consumedFoods, int uneatenFoodIndex, int amountPaid)
+        {
+            consumedFoods.RemoveAt(uneatenFoodIndex);
+            var paymentThatAnnaMustPay = consumedFoods.Aggregate((x, y) => x + y) / 2;
+            var refundAmount = amountPaid - paymentThatAnnaMustPay;
+            return refundAmount > 0 ? refundAmount.ToString() : "Bon Appetit";
+        }
+
+        public static bool Validate(List<int> consumedFoods, int uneatenFoodIndex, int amountPaid)
+        {
+            if (consumedFoods.Count < 2 || consumedFoods.Count > Math.Pow(10, 5)) return false;
+            if (uneatenFoodIndex < 0 || uneatenFoodIndex > consumedFoods.Count) return false;
+            foreach (var number in consumedFoods)
+            {
+                if (number < 0 || number > Math.Pow(10, 4)) return false;
+            }
+            if(amountPaid < 0 || amountPaid > consumedFoods.Aggregate((x, y) => x + y)) return false;
+            return true;
+        }
+    }
+}
---
+++ C#/Algorithms/BillDivision/BillDivisionStart.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.BillDivision
+{
+    public static class BillDivisionStart
+    {
+        public static void Start()
+        {
+            var consumedFoods = new List<int> { 3, 10, 2, 9 };
+            if(BillDivisionService.Validate(consumedFoods, 1, 7))
+                Console.WriteLine(BillDivisionService.Start(consumedFoods, 1, 7));
+        }
+    }
+}
